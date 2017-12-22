0ab81b9640c0d30f02d97621b950b0acb40f4b95
Andrey Bejarano <abejarano@bondacom.com>
2017-12-22 15:41:51 -0300

    feat: change ava to jasmine

--- test/server/cms.service.test.js
+++ test/server/cms.service.test.js
@@ -1,5 +1,5 @@
-const test = require('ava');
 const CmsService = require('../../server/api/cms/cms.service');
+const service = new CmsService();
 const nock = require('nock');
 const stubs = require('./stubs');
 const options = {
@@ -8,19 +8,18 @@ const options = {
 	}
 };
 
-test.beforeEach(t => {
-	t.context.service = new CmsService();
-});
+describe('CMS service', () => {
+	it('GET data check dni', async () => {
+		let affiliate = stubs.getDataAffiliate();
+		const siteId = affiliate.data.company.id;
+		const dni = affiliate.data.member.code;
 
-test('Get data check dni affiliate', async t => {
-	const service = t.context.service;
-	let affiliate = stubs.getDataAffiliate();
-	const siteId = affiliate.data.company.id;
-	const dni = affiliate.data.member.code;
-	t.is(typeof service.getDataAffiliate, 'function');
-	nock(options.endpoints.microsite)
-		.get(`/microsite/${siteId}/affiliates/${dni}`)
-		.reply(200, affiliate);
-	let result = await service.getDataAffiliate({ siteId, dni });
-	t.deepEqual(result, affiliate);
+		expect(typeof service.getDataAffiliate).toBe('function');
+		nock(options.endpoints.microsite)
+			.get(`/microsite/${siteId}/affiliates/${dni}`)
+			.reply(200, affiliate);
+		let result = await service.getDataAffiliate({ siteId, dni });
+
+		expect(result).toEqual(affiliate);
+	});
 });
